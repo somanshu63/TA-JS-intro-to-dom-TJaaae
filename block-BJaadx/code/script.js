45f11f07acc164777fe6bd348ec18d97aac52553
somanshu63 <somanshu63@gmail.com>
2022-06-01 15:55:40 +0530

    correction in learn to select elements

--- block-BJaadx/code/script.js
+++ block-BJaadx/code/script.js
@@ -1,30 +1,27 @@
 // 1. Change the title of the page to `Hello AltCampus!`
-let title = document.querySelector("title");
-title = `Hello AltCampus!`;
+document.title = `Hello AltCampus!`;
 // 2. Select the element using the children property:
 
 //    - Select the `h1` element and change the value to `Learning DOM`
-let h1 = document.body.firstElementChild;
-h1 = `Learning DOM`;
+document.body.children[0].innerText = `Learning DOM`;
 //    - Select the first `li` element inside the `ul` with class `topics` and change the innerText to `all about document`
-let li = document.querySelector(".list");
-li.innerText = "all about document";
+document.body.children[5].children[0].innerText = "all about document";
 //    - Select the input element with name `email`
-let name = document.body.querySelector("#email");
+document.body.body.children[6].children[2];
 // 3. Log the number (using console.log) of children of all the `li` element inside the ul with class `topics`
 console.log(document.body.children[5].children.length);
 // 4. Select the first input using the `type` selector and store them in variable named `emailInput`
 let emailInput = document.getElementById("email");
 // 5. Select the ul element using class selector and store in `topics`
-let topics = document.getElementsByClassName("topics");
+let topics = document.querySelector(".topics");
 // 6. Select the first label element and store in `label`
-let label = document.body.children[6].children[1];
+let label = document.querySelector("label");
 // 7. Select the input of type `checkbox` with the `id` selector and store in `inputCheckbox`
 let inputCheckBox = document.getElementById("remember");
 // 8. Select the input of type password using Attribute selectors. (eg: input[type="text"]) and store in `password`
-let password = document.querySelectorAll(`[type="password"]`);
+let password = document.querySelectorAll(`input[type="password"]`);
 // 9. Select the input using the placeholder attribute selector with value `password` and store in `attrPassword`
-let attrPassword = document.querySelectorAll(`[placeholder="password"]`);
+let attrPassword = document.querySelectorAll(`input[placeholder="password"]`);
 // 10. Select all the `li` element and store in `allTopics`
 let allTopics = document.querySelectorAll("li");
 // 11. Select all the input element of any type and store in `allInput`
@@ -34,13 +31,14 @@ allTopics.forEach((elm) => console.log(elm.innerText));
 // 13. Select all the elements with class `list` and store in variable `listOfSelectedTopics`
 let listOfSelectedTopics =  document.querySelectorAll(".list");
 // 14. Select the first li element inside the `ul` element using `>` (direct child) and store in `firstLi`
-let firstLi = document.body.children[5].children[0];
+let firstLi = document.querySelector("ul>li");
 // 15. Select all the img element and log the number of element saying `The total number of img element is ---`
-console.log(document.querySelectorAll("img").length);
+let allImg = document.querySelectorAll("img").length;
+console.log(` The total number of img element is ${allImg.length} `)
 // 16. Select all the `p` element and store in `allPElement`
 let allPElement = document.querySelectorAll("p");
 // 17. Select all the buttons and log the count of buttons.
-console.log(document.querySelectorAll(`[type="submit"]`).length);
+console.log(document.querySelectorAll(`button`).length);
 // 18. Select all the `label` element and log the count.
 console.log(document.querySelectorAll(`label`).length);
 // 19. Select all the elements with `id` of `test`
